1bac363933ffdc7c72ffde38cc28a4c9ed766380
lotaxgizmo <lotaoko2@gmail.com>
2024-11-22 07:34:40 +0100

    Update

--- src/components/Contract.js
+++ src/components/Contract.js
@@ -10,7 +10,7 @@ function Contract() {
     const copyTextToClipboard = () => {
         // Create a temporary textarea element
         const textarea = document.createElement('textarea');
-        textarea.value = '7xEdKtj6nX2nvqPGayLi4egSWwr53NYSaVZQLRLapump'; // Replace with your actual text to copy
+        textarea.value = '58WKuKzAH6A5QHdBAT8n2QHiFtzXcPXBEEXB785Ppump'; // Replace with your actual text to copy
         textarea.setAttribute('readonly', '');
         textarea.style.position = 'absolute';
         textarea.style.left = '-9999px'; // Move outside the screen to make it invisible
@@ -30,10 +30,16 @@ function Contract() {
             <div className='bg-[#FFD401] flex flex-col justify-center items-center rounded-xl lg:rounded-3xl my-10 w-11/12  lg:w-9/12 lg:p-4d  overflow-clip.  border-black border-2 p-4 lg:p-24 relative'>
                 <img src={contractasd} alt="" />
 
-                <div className="relative  w-auto copy flex flex-col lg:flex-row justify-center items-center text-wrap ">
-                    <img src={caaa} alt="" />
+                <div className="relative  lg:w-auto w-11/12 copy flex flex-col lg:flex-row justify-center items-center text-wrap ">
+                    {/* <img src={caaa} alt="" /> */}
+                    <p className='text-sm lg:text-3xl break-words text-center lg:w-auto w-11/12'>58WKuKzAH6A5QHdBAT8n2QHiFtzXcPXBEEXB785Ppump</p>
                     {/* <p className='break-words overflow-wrap text-sm lg:text-3xl text-wrap text-center  '>7xEdKtj6nX2nvqPGayLi4egSWwr53NYSaVZQLRLapump</p> */}
-                    <img src={copybtn} alt="" className='m-2' onClick={copyTextToClipboard} />
+                    <img
+                        src={copybtn}
+                        alt=""
+                        className='m-2 transition-transform duration-200 ease-in-out transform hover:scale-110 active:scale-95'
+                        onClick={copyTextToClipboard}
+                    />
                 </div>
             </div>
         </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,10 +2,13 @@ import React from 'react'
 import heroimg from '../assets/heroimg.svg'
 import herotxt from '../assets/herotxt.svg'
 import bannertxt from '../assets/bannertxt.png'
+import website from '../assets/website.svg'
 
 function Hero() {
     return (
         <div className='flex flex-col justify-center items-center' id='home'>
+            <img src={website} alt="" className='lg:w-32 w-20' />
+
             <img src={herotxt} alt="" className='' />
             <img src={heroimg} alt="" className='w-[1300px] lg:-m-40 -m-10' />
             <img src={bannertxt} alt="" className='w-full -m-10 lg:-m-52' />
